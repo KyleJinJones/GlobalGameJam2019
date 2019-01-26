df36949c86e96f278db2cca895a90d4025b347c4
Kyle Jin Jones <kjjones1@uci.edu>
2019-01-26 15:13:00 -0800

    slipper+ Mainmenu + TextMeshPro

---
+++ GlobalGameJam2019/Assets/Scripts/UiFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UiFunctions : MonoBehaviour
+{
+    public void ChangeScene(int s){
+        SceneManager.LoadScene(s);
+        }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
