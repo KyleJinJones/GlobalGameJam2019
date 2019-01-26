78c099ffd4c06b6372e4a678db56ec1214055f1c
Asher Chang <asherchang13@hotmail.com>
2019-01-26 15:06:20 -0800

    PlayerMovement

--- GlobalGameJam2019/Assets/Scripts/PlayerMovement.cs
+++ GlobalGameJam2019/Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,88 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float Maxspeed = 10.0f;
+    [SerializeField] private float Minspeed = 0f;
+    [SerializeField] private float accerleration = 1f;
+ 
+    private float x_dir = 0;
+    private float z_dir = 0;
+    private float pre_x_dir = 0;
+    private float pre_z_dir = 0;
+
     void Start()
     {
         rb=GetComponent<Rigidbody>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))) * speed;
+        x_dir = Input.GetAxisRaw("Horizontal");
+        z_dir = Input.GetAxisRaw("Vertical");
+
+        if (x_dir != 0) pre_x_dir = x_dir;
+        
+        if (z_dir != 0) pre_z_dir = z_dir;
+        
     }
-   
+
+    private void FixedUpdate()
+    {
+        if (x_dir != 0 || z_dir != 0)
+        {
+            Minspeed = Approach(Minspeed, Maxspeed, accerleration);
+        }
+        else
+        {
+            Minspeed = 0;
+        }
+
+        speed = Minspeed;
+        rb.velocity = Vector3.Normalize(new Vector3(x_dir, 0, z_dir)) * speed;
+        Debug.Log(speed);
+    }
 
     private void OnTriggerStay(Collider other)
     {
         
     }
+
+    public float Approach(float a, float b, float c)
+    {
+        if (a < b)
+        {
+            a += c;
+            if (a > b)
+                return b;
+        }
+        else
+        {
+            a -= c;
+            if (a < b)
+                return b;
+        }
+        return a;
+
+    }
+    
+    public float Sign(float a)
+    {
+        if(a != 0)
+        {
+            if(a > 0)
+            {
+                return 1;
+            }
+
+            if (a < 0)
+            {
+                return -1;
+            }
+
+        }
+        
+        return  0;
+    }
 }
+
