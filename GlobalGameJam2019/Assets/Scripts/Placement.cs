14a4c51a9139488a212b14bd10bce4917eb00a6b
Kyle Jin Jones <kjjones1@uci.edu>
2019-01-26 02:38:56 -0800

    Initial PRoject Commit: Player Movement and Mesh Coloration

---
+++ GlobalGameJam2019/Assets/Scripts/Placement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Placement : MonoBehaviour
+{
+    private Renderer r;
+    public Color c;
+    private void Start()
+    {
+        r = this.GetComponent<Renderer>();    
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            r.material.color = c;
+        }
+    }
+}
---
+++ GlobalGameJam2019/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float speed = 5.0f;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal")*speed, 0, Input.GetAxis("Vertical") * speed);
+    }
+}
