baae81edddc5717ba2be64a52588d7d57fd6dd87
luyao <luyaoz3283@gmail.com>
2019-01-26 14:03:23 -0800

    Auto stash before merge of "master" and "origin/master"

--- GlobalGameJam2019/Assets/Scripts/Inventory.cs
+++ GlobalGameJam2019/Assets/Scripts/Inventory.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public GameObject carried = null;
+    public GameObject carried;
+    public float scale;
+
+    void Update()
+    {
+        if (carried != null)
+        {
+            Vector3 velocity = this.GetComponent<Rigidbody>().velocity;
+            if (velocity.magnitude != 0)
+            {
+                Vector3 displacement = Quaternion.AngleAxis(90, new Vector3(0, 1, 0)) * (scale * velocity);
+                carried.transform.position = this.transform.position + displacement / (displacement.magnitude/1.25f);
+            }
+            
+        }
+        
+    }
 }
