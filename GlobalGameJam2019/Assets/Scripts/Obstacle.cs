b97855db1a9f261861c312bf5be243068f45f674
Kyle Jin Jones <kjjones1@uci.edu>
2019-01-26 17:21:52 -0800

    Scene Transitions and basic obstacles

---
+++ GlobalGameJam2019/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private float obstaclespeed = 5.0f;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = (new Vector3(obstaclespeed, 0, 0));
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
