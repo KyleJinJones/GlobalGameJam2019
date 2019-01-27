7809e12df6c890a365beff31e07617da1a32bbbc
Kyle Jin Jones <kjjones1@uci.edu>
2019-01-27 12:29:02 -0800

    PickupSounds, lvl updates

--- GlobalGameJam2019/Assets/Scripts/Inventory.cs
+++ GlobalGameJam2019/Assets/Scripts/Inventory.cs
@@ -6,29 +6,45 @@ public class Inventory : MonoBehaviour
 {
     public GameObject carried;
     public float scale;
-
+    AudioSource a;
+    private bool nothing = true;
+    private void Start()
+    {
+        a = GetComponent<AudioSource>();
+    }
     void Update()
     {
         if (carried != null)
         {
+            
             Vector3 velocity = this.GetComponent<Rigidbody>().velocity;
             if (velocity.magnitude != 0)
             {
                 Vector3 displacement = Quaternion.AngleAxis(90, new Vector3(0, 1, 0)) * (scale * velocity);
                 carried.transform.position = this.transform.position + displacement / (displacement.magnitude/1.25f) + Vector3.up;
             }
+            if (nothing)
+            {
+                a.Play();
+                nothing = false;
+            }
             
         }
-        
+        else
+        {
+            nothing = true;
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("test:" + other.tag);
+        //Debug.Log("test:" + other.tag);
         if ((other.tag == "material" || other.tag == "resource" )&& carried == null)
         {
             
             carried = other.gameObject;
+            a.Play();
 
         }
     }
