286ac71e3f79a8d01fb7334772fa8e02d7379d62
luyao <luyaoz3283@gmail.com>
2019-01-26 19:47:50 -0800

    added a crafting station

---
+++ GlobalGameJam2019/Assets/Scripts/item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    //public enum typeList { wood, stone };
+    public recipe.resourceNameList resourceType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
